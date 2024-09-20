50b1600835d91c0aa3afe5f30a99b80b6680fb49
Madhuri Sorte <sortemadhurid@gmail.com>
2024-09-20 22:36:41 +0530

    added validator

--- p9_feedback/testapp/forms.py
+++ p9_feedback/testapp/forms.py
@@ -1,15 +1,17 @@
 from django import forms
+from django.core import validators
 
 class FeedbackForm(forms.Form):
     name = forms.CharField()
     rollno = forms.IntegerField()
     email = forms.EmailField()
-    feedback = forms.CharField(widget=forms.Textarea)
+    feedback = forms.CharField(widget=forms.Textarea, 
+                               validators=[validators.MaxLengthValidator(40)]) # parameter to validate field
     
-    def clean_name(self):   # clean_ name is fixed, django callled this method automatically
-        inputname = self.cleaned_data['name'] # cleaned_date is a dictionary ... fixed name
-        print("*****validating name field*******")
-        if len(inputname)<4:
-            raise forms.ValidationError("char should be greater than 4")
-        return inputname
+    # def clean_name(self):   # clean_ name is fixed, django callled this method automatically
+    #     inputname = self.cleaned_data['name'] # cleaned_date is a dictionary ... fixed name
+    #     print("*****validating name field*******")
+    #     if len(inputname)<4:
+    #         raise forms.ValidationError("char should be greater than 4")
+    #     return inputname
     
