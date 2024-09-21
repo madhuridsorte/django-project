82a638b7f15ea664847a19acd259d8b1f4a4ec6e
Madhuri Sorte <sortemadhurid@gmail.com>
2024-09-21 12:27:49 +0530

    added own validator

--- p9_feedback/testapp/forms.py
+++ p9_feedback/testapp/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 from django.core import validators
 
+def start_with_d(value):
+    print("************inside start_with_d***************")
+    if value[0].lower()!='d':
+        raise forms.ValidationError("name should be start with d or D")
+def gmail_validator(value):
+    print("************inside gmail_validator***************")
+    if value[-10:] !="@gmail.com":
+        raise forms.ValidationError("mail extension should be @gmail.com ")
+        
+
 class FeedbackForm(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[start_with_d]) # created our own validator 
     rollno = forms.IntegerField()
-    email = forms.EmailField()
+    email = forms.EmailField(validators=[gmail_validator])
     feedback = forms.CharField(widget=forms.Textarea, 
                                validators=[validators.MaxLengthValidator(40)]) # parameter to validate field
     
